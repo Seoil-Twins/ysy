a4ae7fb1d49cb4e80925de157ee40d2e75c90850
seungyong <seungyong20@naver.com>
2023-01-05 21:57:41 +0900

    Conflict Error 추가
    1. 중복된 ID, Code, Email 등을 위한 Custom Error

--- backend/src/util/statusCode.ts
+++ backend/src/util/statusCode.ts
@@ -7,9 +7,19 @@ const StatusCode = {
     "FORBIDDEN": 403,
     "NOT_FOUND": 404,
     "METHOD_NOT_ALLOWED": 405,
+    "CONFLICT": 409,
     "INTERNAL_SERVER_ERROR": 500
 } as const;
 
-type StatusCode = typeof StatusCode[keyof typeof StatusCode];
+/*
+    keyof typeof StatusCode
+    key의 typeof 값을 가져오는데 as const로 인해서 키의 타입 값들이 string이 아닌 OK, CREATED처럼 나옴.
+    
+    typeof StatusCode[keyof typeof StatusCode]
+    type에는 자바스크립트 타입만 들어갈 수 있기때문에 typeof를 선언
+    object["OK"] = 200
+    이걸 union 방식으로 뿌려줌 (200 | 201 | ...)
+*/
+// type StatusCode = typeof StatusCode[keyof typeof StatusCode];
 
 export default StatusCode;
