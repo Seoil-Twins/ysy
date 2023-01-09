cfa469eeac8059916e7adba97fb4bf3842a06536
seungyong <seungyong20@naver.com>
2023-01-09 20:22:04 +0900

    회원가입 시 중복된 유저  에러 발생
    1. 회원가입 시 중복된 유저가 있을 시 500에러 발생한 걸 409 에러로 변경

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import AbstractError from "../error/abstractError";
+import ConflictError from "../error/conflict";
 import StatusCode from "../util/statusCode";
 
 const globalErrorHandler: ErrorRequestHandler = (e: any, req: Request, res: Response, next: NextFunction) => {
@@ -8,8 +9,11 @@ const globalErrorHandler: ErrorRequestHandler = (e: any, req: Request, res: Resp
     if (e instanceof AbstractError) {
         const { message, statusCode } = e;
         res.status(statusCode || StatusCode.INTERNAL_SERVER_ERROR).json({ message });
+    } else if (req.url === "/user" && req.method === "POST" && e.errno === 1062) {
+        // 1062 오류가 여러 개의 상황이 중첩되면 else if 안에서 url과 method를 나누기
+        const { statusCode, message } = new ConflictError("Duplicated User");
+        res.status(statusCode).json({ message });
     } else {
-        // 라이브러리 등 에러 발생시...
         res.status(StatusCode.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
     }
 };
