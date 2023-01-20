c33d1160d969ef93bb3f6132c61b5321852d46f9
seungyong <seungyong20@naver.com>
2023-01-20 20:24:28 +0900

    Joi cupId Error 수정
    1. auth middleware를 통해 request body에 cupId를 추가를 했었으나 Joi validate에서 유효하지 않은 request body라고 오류가 발생
    
    2. allowUnknown을 추가해 기존을 제외한 다른 값이 와도 무시하도록 변경

--- backend/src/util/validator.ts
+++ backend/src/util/validator.ts
@@ -3,6 +3,7 @@ import joi from "joi";
 const validator = (payload: JSON, schema: joi.Schema) => {
     const result: joi.ValidationResult = schema.validate(payload, {
         abortEarly: false,
+        allowUnknown: true
     });
 
     return result;
