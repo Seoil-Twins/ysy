aca35f8ffee2490d524e81e252c759e5a59e226e
seungyong <seungyong20@naver.com>
2023-03-26 20:28:12 +0900

    middleware에 errorCode 추가

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -3,7 +3,7 @@ import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import AbstractError from "../error/abstract.error";
 import logger from "../logger/logger";
 
-import { STATUS_CODE } from "../constant/statusCode.constant";
+import { ERROR_CODE, STATUS_CODE } from "../constant/statusCode.constant";
 
 const globalErrorHandler: ErrorRequestHandler = (e: any, req: Request, res: Response, next: NextFunction) => {
     logger.debug(`Error Handler => ${e}`);
@@ -13,14 +13,16 @@ const globalErrorHandler: ErrorRequestHandler = (e: any, req: Request, res: Resp
         res.status(statusCode || STATUS_CODE.INTERNAL_SERVER_ERROR).json({
             type: e.name,
             message: message,
-            statusCode: e.statusCode
+            statusCode: e.statusCode,
+            errorCode: e.errorCode
         });
     } else {
         logger.error(`Server Error : ${JSON.stringify(e)} ${e.stack}`);
         res.status(STATUS_CODE.INTERNAL_SERVER_ERROR).json({
             type: "Internal Server Error",
             message: "Unknown Error",
-            statusCode: STATUS_CODE.INTERNAL_SERVER_ERROR
+            statusCode: STATUS_CODE.INTERNAL_SERVER_ERROR,
+            errorCode: ERROR_CODE.INTERNAL_SERVER_ERROR
         });
     }
 };
