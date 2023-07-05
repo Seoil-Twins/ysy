37a0a243cce93c4baf6999e43d9f286a1e3d8f50
seungyong <seungyong20@naver.com>
2023-07-05 22:18:55 +0900

    redux 및 secureStore 추가
    1. Redux 추가
    
    2. SecureStore 추가

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,46 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { Provider } from "react-redux";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 
-SplashScreen.preventAutoHideAsync();
+import store from "./redux/store";
+import { useAppSelector, useAppDispatch } from "./redux/hooks";
+import { login, logout } from "./features/loginStatusSlice";
 
 import Nav from "./navigation/Nav";
 
+SplashScreen.preventAutoHideAsync();
+
+const AppWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
 const App = () => {
+  const isLogin = useAppSelector((state) => state.loginStatus.isLogin);
+  const dispatch = useAppDispatch();
+
+  const onLogin = () => {
+    dispatch(login());
+  };
+
+  const onLogout = () => {
+    dispatch(logout());
+  };
+
+  // 컴포넌트 이동마다 로그인 상태 확인
+  useEffect(() => {}, []);
+
+  // 로그인 상태가 변할 때 마다
+  useEffect(() => {
+    console.log(isLogin);
+    onLogin();
+  }, [isLogin]);
+
   const [fontsLoaded] = useFonts({
     "notosans-medium": require("./assets/fonts/NotoSansKR-Medium.otf"),
     "notosans-regular": require("./assets/fonts/NotoSansKR-Regular.otf")
@@ -36,8 +69,7 @@ const App = () => {
 const styles = StyleSheet.create({
   safeContainer: {
     flex: 1
-    // marginTop: StatusBar.currentHeight
   }
 });
 
-export default App;
+export default AppWrapper;
