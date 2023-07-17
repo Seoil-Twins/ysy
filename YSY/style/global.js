99369709503c607229cfade0ab68948aabd7a043
seungyong <seungyong20@naver.com>
2023-07-17 16:34:21 +0900

    Back Header 추가
    1. 뒤로가기 헤더 추가

--- YSY/style/global.js
+++ YSY/style/global.js
@@ -3,7 +3,8 @@ import { StyleSheet } from 'react-native';
 export const globalStyles = StyleSheet.create({
   mlmr20: {
     flex: 1,
-    marginLeft: 20,
-    marginRight: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#FFFFFF',
   },
 });
