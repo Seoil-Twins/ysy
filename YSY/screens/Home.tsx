9991bc6187ccd23b6e51d43d0187e80390120bc1
seungyong <seungyong20@naver.com>
2023-07-26 17:17:06 +0900

    Home 완성
    1. ImagePicker 적용
    
    2. DatePicker 적용
    
    3. cupDay 계산
    
    4. UI 완성

--- YSY/screens/Home.tsx
+++ YSY/screens/Home.tsx
@@ -1,5 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, ImageBackground, Image } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Image,
+  Pressable,
+} from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import ImageCropPicker, { ImageOrVideo } from 'react-native-image-crop-picker';
 
 import CustomText from '../components/CustomText';
 
@@ -9,17 +17,22 @@ import { globalStyles } from '../style/global';
 
 import DefaultPersonSVG from '../assets/icons/person.svg';
 import LoveSVG from '../assets/icons/small_love.svg';
+import CalendarSVG from '../assets/icons/calendar_lightgray.svg';
+import PickImageSVG from '../assets/icons/pick_image.svg';
 
 const Home = () => {
   const [cupInfo, setCupInfo] = useState<Couple | undefined>(undefined);
+  const [day, setDay] = useState<number>(0);
+  const [cupDay, setCupDay] = useState<Date | undefined>(undefined);
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+  const [datePickerVisible, setDatePickerVisible] = useState<boolean>(false);
 
   const getCoupleInfo = async () => {
-    const response: Couple = {
+    const response: Couple = await {
       cupId: 'gPz9fLmw',
       cupDay: '2023-01-17',
       title: '커플 제목2',
-      thumbnail:
-        'https://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+      thumbnail: null,
       createdTime: '2023-01-23T05:03:49.000Z',
       users: [
         {
@@ -54,16 +67,125 @@ const Home = () => {
       ],
     };
 
+    setThumbnail(response.thumbnail);
+    setCupDay(new Date(response.cupDay));
     setCupInfo(response);
   };
+
+  const getServerTime = async () => {
+    const serverTime = new Date('2023-07-26 15:55');
+    const response = new Date(
+      `${serverTime.getFullYear()}-${
+        serverTime.getMonth() + 1
+      }-${serverTime.getDate()}`,
+    );
+
+    return response;
+  };
+
+  const daysDiff = (current: Date, cupDay: Date) => {
+    const timeDiff = cupDay.getTime() - current.getTime();
+    // 밀리초를 일수로 변환 (1일 = 24시간 * 60분 * 60초 * 1000밀리초)
+    const diff = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
+    return Math.abs(diff);
+  };
+
+  const calculateDay = useCallback(async () => {
+    if (cupDay) {
+      const current: Date = await getServerTime();
+      const cupDayToDate: Date = new Date(cupDay);
+      const days: number = daysDiff(current, cupDayToDate);
+
+      setDay(days);
+    }
+  }, [cupDay]);
 
   useEffect(() => {
     getCoupleInfo();
   }, []);
 
+  useEffect(() => {
+    calculateDay();
+  }, [cupDay, calculateDay]);
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const updateCupDay = async (_date: Date) => {
+    // 업데이트 커플 데이
+    const response = 201;
+    return response;
+  };
+
+  const handleConfirm = async (date: Date) => {
+    const response = await updateCupDay(date);
+
+    if (response === 201) {
+      setCupDay(date);
+    } else {
+      console.log('error');
+    }
+
+    hideDatePicker();
+  };
+
+  const updateThumbnail = async (_image: ImageOrVideo) => {
+    const response = 201;
+    return response;
+  };
+
+  const onSuccess = async (image: ImageOrVideo) => {
+    // Formdata를 통해 전송
+    /**
+     * {
+        "cropRect":{
+            "height":2857,
+            "width":1713,
+            "x":1168,
+            "y":1
+        },
+        "height":600,
+        "mime":"image/jpeg",
+        "modificationDate":"1689677165000",
+        "path":"file:///storage/emulated/0/Android/data/com.ysy/files/Pictures/3526149c-84b9-4ab6-92ec-a3943e1bde4b.jpg",
+        "size":194997,
+        "width":360
+       }
+     */
+    await updateThumbnail(image);
+    setThumbnail(image.path);
+  };
+
+  const onError = (error: any) => {
+    if (String(error).includes('User cancelled image selection')) {
+      console.log('user cancel');
+    } else if (String(error).includes('permission')) {
+      console.log('Not Allow permission');
+    }
+  };
+
+  const showImagePicker = () => {
+    ImageCropPicker.openPicker({
+      width: 360,
+      height: 640,
+      cropping: true,
+    })
+      .then(onSuccess)
+      .catch(onError);
+  };
+
   return (
     <ImageBackground
-      source={require('../assets/icons/main_background.png')}
+      source={
+        thumbnail
+          ? { uri: thumbnail }
+          : require('../assets/icons/main_background.png')
+      }
       resizeMode="cover"
       style={[styles.container, globalStyles.mlmr20]}>
       <View style={styles.titleBox}>
@@ -72,7 +194,7 @@ const Home = () => {
         </CustomText>
         <View style={styles.titleRow}>
           <CustomText size={30} weight="medium" color="#FF6D70">
-            456
+            {day}
           </CustomText>
           <CustomText size={30} weight="regular" color="#FFFFFF">
             일
@@ -112,6 +234,25 @@ const Home = () => {
           </CustomText>
         </View>
       </View>
+      <View style={styles.funcBox}>
+        <Pressable
+          style={[styles.funcItem, styles.mr20]}
+          onPress={showDatePicker}>
+          <CalendarSVG />
+        </Pressable>
+        <Pressable style={styles.funcItem} onPress={showImagePicker}>
+          <PickImageSVG />
+        </Pressable>
+      </View>
+      <DateTimePickerModal
+        isVisible={datePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        locale="ko_KR"
+        date={cupDay ? cupDay : new Date()}
+        maximumDate={new Date()}
+      />
     </ImageBackground>
   );
 };
@@ -121,7 +262,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   titleBox: {
-    flex: 2,
+    flex: 1,
     alignItems: 'center',
     marginTop: 70,
   },
@@ -129,11 +270,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   userBox: {
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 15,
+    marginBottom: 25,
   },
   userItemBox: {
     alignItems: 'center',
@@ -159,6 +299,22 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignSelf: 'center',
     bottom: 0,
+  },
+  funcBox: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginBottom: 15,
+  },
+  funcItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 40 / 2,
+    backgroundColor: '#0000004D',
+  },
+  mr20: {
+    marginRight: 20,
   },
 });
 
