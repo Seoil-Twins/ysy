d1244545da2c042f8002841d7f123bc0cc94c14d
seungyong <seungyong20@naver.com>
2023-08-21 20:47:21 +0900

    FAQ 완성
    1. FAQ 완성

--- YSY/screens/FAQ.tsx
+++ YSY/screens/FAQ.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { SettingsNavType } from '../navigation/NavTypes';
+
+import RenderHtml from '../components/RenderHtml';
+
+import { globalStyles } from '../style/global';
+import { FAQ as FAQType } from '../types/faq';
+
+import BackHeader from '../components/BackHeader';
+import CustomText from '../components/CustomText';
 
 const FAQ = () => {
+  const faq: FAQType =
+    useRoute<RouteProp<SettingsNavType, 'FAQ'>>().params.info;
+
   return (
-    <View>
-      <Text>FAQ</Text>
+    <View style={globalStyles.mlmr20}>
+      <BackHeader style={styles.container} />
+      <CustomText size={26} weight="medium" style={styles.title}>
+        {faq.title}
+      </CustomText>
+      <RenderHtml html={faq.contents} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 25,
+  },
+  title: {
+    marginBottom: 20,
+  },
+});
 
 export default FAQ;
